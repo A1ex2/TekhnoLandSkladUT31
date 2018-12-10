8196bb96a9a7f87ff2d0d370f7a3aed2877dae36
A1ex <jaroshov@ukr.net>
2018-12-10 16:06:49 +0200

    10122018

--- app/src/main/java/android/a1ex/com/tekhnolandskladut31/Password.java
+++ app/src/main/java/android/a1ex/com/tekhnolandskladut31/Password.java
@@ -1,17 +1,13 @@
 package android.a1ex.com.tekhnolandskladut31;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Password extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(this, Password.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_password);
     }
 }

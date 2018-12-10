3e6626ea31283a12d49a3321a8d445e143aa48a9
A1ex <jaroshov@ukr.net>
2018-12-10 16:21:58 +0200

    10122018

--- app/src/main/java/android/a1ex/com/tekhnolandskladut31/Password.java
+++ app/src/main/java/android/a1ex/com/tekhnolandskladut31/Password.java
@@ -2,12 +2,31 @@ package android.a1ex.com.tekhnolandskladut31;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Password extends AppCompatActivity {
+    private EditText login;
+    private EditText password;
+    private Button ok;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password);
+
+        login = findViewById(R.id.login);
+        password = findViewById(R.id.password);
+        ok = findViewById(R.id.butt_OK);
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Все ОК", Toast.LENGTH_LONG);
+            }
+        });
+
     }
 }

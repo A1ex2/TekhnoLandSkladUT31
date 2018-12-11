9639a14e3fde0ec2857c174321979a38dd8126d8
A1ex <jaroshov@ukr.net>
2018-12-11 09:37:25 +0200

    11122018

---
+++ app/src/main/java/android/a1ex/com/tekhnolandskladut31/DocumentResidueEntry.java
@@ -0,0 +1,13 @@
+package android.a1ex.com.tekhnolandskladut31;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DocumentResidueEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_document_residue_entry);
+    }
+}
--- app/src/main/java/android/a1ex/com/tekhnolandskladut31/MainActivity.java
+++ app/src/main/java/android/a1ex/com/tekhnolandskladut31/MainActivity.java
@@ -4,8 +4,13 @@ import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public Button add_doc;
+    public Button list_doc;
+    public Button update_directories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +19,32 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, Password.class);
         startActivityForResult(intent, 1);
+
+        add_doc = findViewById(R.id.add_doc);
+        add_doc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DocumentResidueEntry.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+
+        list_doc = findViewById(R.id.list_doc);
+        list_doc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Password.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+
+        update_directories = findViewById(R.id.update_directories);
+        update_directories.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     @Override
